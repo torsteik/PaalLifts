8dd8d3374fd8deb1161112cdc987d88f9ddb12b1
torsteik <tkvalvaag94@hotmail.com>
2016-04-05 17:44:09 +0200

    Update hei.c

--- Lifts/Pseudo/hei.c
+++ Lifts/Pseudo/hei.c
@@ -1,19 +1,17 @@
-//FSM_HEIS
-//main.c
+
 global ready = 0;
 global network_ready = 0
 
-//init{
 int main(){
 	start(local);
-	while(!ready){}
+	while (!ready){}
 
 	ready = 0;
 	start(network);
-	while(!ready){}
+	while (!ready){}
 
-	/* 	... 
-		...
+	/* 	...
+	...
 	*/
 	select()
 }

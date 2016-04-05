986c60215f6f4a58671d88249afab4ac8127a3d8
torsteik <tkvalvaag94@hotmail.com>
2016-04-05 17:43:01 +0200

    Update network.c

--- Lifts/Pseudo/network.c
+++ Lifts/Pseudo/network.c
@@ -1,37 +1,67 @@
-#include defines.c
+#include defines.h
 
+//Set initial state
 state cur_state = FIND_NETWORK;
+substate_m cur_substate_m = SETUP;
+//Backup starts as an invalid ID
 uint8_t backup = NO_IP;
-
+//List of what IP's are included in our network
 uint8_t ID_list[256];
 
 
-while(1){
-	switch (state){
-		case FIND_NETWORK:
-			if(!find_master()){ state = MASTER; }
-			else			  {	state = request_state(); }
+while (1){
+	switch (cur_state){
+	case FIND_NETWORK:
+		if (!find_master()){ cur_state = MASTER; }
+		else			   { cur_state = SLAVE; }
+		break
+
+	case MASTER:
+		switch (cur_substate_m){
+		case SETUP: //Start threads
+			start(recieve_connection);
+			start(broadcast_IAMALIVE)
+			...
+			/*
+			PLACE THIS SOMEWHERE
+			if (backup == NO_IP){
+				find_backup();
+				start(backup_com);
+			} 
+			//When a dead backup is found backup will be set to NO_IP and backup thread will be closed
+			*/
 			break
 
-		case MASTER:
-			switch(substate_m){
-				case SETUP:
-					start(recieve_connection);
-					...
-					break
-					
-				case RUN:
-					broadcast_IMALIVE();
-					update_backup();
-					...
-					break
+		case RUN: //Check shared variables to see if we want to change state
+			if (network_error){
+				cur_substate_m = CLOSE_NETWORK;
+				break
 			}
+			...
 			break
 
-		case BACKUP:
+		case CLOSE_NETWORK:	
+			broadcast_death(); //Signal shutdown of network to participant
+			/*
+			Close threads and sockets
+			*/
+			cur_substate_m = SETUP;
+			cur_state_m = FIND_NETWORK;
 			break
+		}
 
-		case SLAVE:
+	case SLAVE:
+		switch (cur_substate_s){
+		case SETUP:
+			
 			break
+		
+		case BACKUP: //Only for setting up threads, need seperate way to indicate that this is a backup
+	
+			break
+		}
+		break
 	}
 }
+
+

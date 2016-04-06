4fcaee7ef9fb35f960481112f6cb1321ac0dcb71
torsteik <tkvalvaag94@hotmail.com>
2016-04-06 12:05:12 +0200

    Update and rename hei.c to main.cpp

--- Lifts/Pseudo/main.cpp
+++ Lifts/Pseudo/main.cpp
@@ -1,17 +1,19 @@
-
 global ready = 0;
 global network_ready = 0
 
 int main(){
+
+	{
 	start(local);
 	while (!ready){}
 
 	ready = 0;
 	start(network);
 	while (!ready){}
-
 	/* 	...
 	...
 	*/
+	}
+	
 	select()
 }

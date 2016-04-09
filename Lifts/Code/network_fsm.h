aa7e4cb291a11fddb5b5a738e33ca537a83bb033
torsteik <tkvalvaag94@hotmail.com>
2016-04-09 20:35:20 +0200

    Update network_fsm.h

--- Lifts/Code/network_fsm.h
+++ Lifts/Code/network_fsm.h
@@ -1,11 +1,30 @@
 #ifndef NETWORK_FSM_H
 #define NETWORK_FSM_H
 
-#define STATUS_PORT 32000
-#define 
-// Type definitions: (Maybe move this to seperate file set for code quality.)
-enum state_t{ FIND_NETWORK, MASTER, SLAVE};
+#define SEC_TO_USEC		1000000
+
+#define BROADCAST		255
+
+#define NETW_INFO_PORT	32123
+#define ELEV_INFO_PORT	32124
+
+#define MSG_ID			(int)msg.content[0] //Consider making enum && move to network_fsm.h
+//OBS OBS OBS, MSG_ID only works if the "message" is called msg.
+#define ERROR			0
+#define	M_ALIVE			1 //Consider making this 3 so it sends hearts
+#define CONNECT			2
+
+// Type definitions:
+enum state_t{ FIND_NETWORK, MASTER, SLAVE };
+struct manage_connections_arg_t{
+
+};
 
 // Functions:
+void network_fsm();
 int find_master();
+	//The functions to be run by threads should probably have their own module or something; maybe there should be a module for each state: master, slave
+void* broadcast_alive_func(void* netw_info_sock_void_ptr);
+void* manage_connections_func(void* netw_info_sock_void_ptr);
+int determine_master();
 #endif

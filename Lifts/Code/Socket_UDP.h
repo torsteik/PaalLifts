89fe7c1189316a3823b43853c58760187157e49c
torsteik <tkvalvaag94@hotmail.com>
2016-04-09 20:35:42 +0200

    Update Socket_UDP.h

--- Lifts/Code/Socket_UDP.h
+++ Lifts/Code/Socket_UDP.h
@@ -1,29 +1,29 @@
 #ifndef SOCKET_UDP_H
 #define SOCKET_UDP_H
 
-enum msg_id{
-	M_ALIVE
-};
+#define BUFF_SIZE	1024
 
-struct msg_t{
-	int sender;
-	msg_id id;
-	char content[1024];
+struct msg_t{ //Make a member of Socket_UDP
+	int sender_ip;
+	char content[BUFF_SIZE];
 };
 
 class Socket_UDP{
 public:
-	Socket_UDP(int ip_addr, int port);
-	Socket_UDP(struct sockaddr* dest_addr_in);
+	Socket_UDP();
+	Socket_UDP(int recv_ip, int send_ip, int port);
+	Socket_UDP(struct sockaddr* dest_addr_in); //Remove, but maybe make 3rd option where there is no recv ip so no recv addr is created
 	~Socket_UDP();
 
 	int pid;
-	char buffer[1024]; //Change to private? Adjust size?
+	char buffer[BUFF_SIZE]; //Change to private? Adjust size?
 
 	msg_t recv();
+	int send(char content[BUFF_SIZE]);
 
 private:
-	struct sockaddr_in dest_addr; //Not a good name
+	struct sockaddr_in recv_addr; //Consider changing name to bind_addr or something
+	struct sockaddr_in send_addr;
 
 	void sock_setup(); //Review
 };

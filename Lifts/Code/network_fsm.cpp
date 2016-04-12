cb0b96f9e7be305f594720069e0ddd831866c743
torsteik <tkvalvaag94@hotmail.com>
2016-04-12 20:50:49 +0200

    Update network_fsm.cpp

--- Lifts/Code/network_fsm.cpp
+++ Lifts/Code/network_fsm.cpp
@@ -1,46 +1,38 @@
-//Network
 #include <netinet/ip.h>
 #include <sys/time.h>
 #include <sys/select.h>
-//Threading
 #include <pthread.h>
-//Other
 #include <unistd.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdint.h>
 
-#include "network_fsm.h"
+#include "netw_fsm.h"
 #include "netw_master.h"
-#include "Socket_UDP.h"
+#include "NetwMemb.h"
 
 void network_fsm(){
-	state_t state = FIND_NETWORK;
-	
-	Socket_UDP netw_members[256]; //List of members of the network. Members are id'd by their IP and broadcast's ID is taken by an INADDR_ANY socket
-	netw_members[255] = Socket_UDP(INADDR_ANY, BROADCAST, NETW_INFO_PORT, BROADCAST_SOCK);
-	
-	char master_q[N_FLOORS*2];
-	for (int i = 0; i < N_FLOORS * 2; i++)	master_q[i] = 255; //OBS 255, not 0
+	SharedVars shrd_vars;																//Should maybe be named shrd_master_vars or something
+	shrd_vars.netw_membs[BROADCAST] = NetwMemb(INADDR_ANY, id_to_ip(BROADCAST), NETW_INFO_PORT, BROADCASTER); /*Review: OBS! BROADCAST need to be full IP in host byte order and BROADCASTER must be the lamest define ever, lol*/
+	for (int i = 0; i < N_FLOORS * 2; i++)	shrd_vars.master_q[i] = 0;
 
-	
+	unsigned long master_ip;
 
+	State state = FIND_NETWORK;
 	while (1){
 		switch (state){
 		case FIND_NETWORK:
-			if (!find_master(netw_members)) state = MASTER; //OBS, make sure ip = .0 is not part of network or do i want this to be possible and instead use 255 as the false value
-			else 							state = SLAVE;
+			master_ip = find_master(shrd_vars.netw_membs);
+			if (!master_ip) state = MASTER; 
+			else 			state = SLAVE;
 			break;
 
 		case MASTER:
-			pthread broadcast_alive_thread; //Consider making a pthread array; would make naming simpler. I second this!
-			pthread manage_connections_thread;
-			pthread manage_backup_thread;
-			pthread distribute_orders_thread;
-			
-			pthread_create(&broadcast_alive_thread, NULL, &broadcast_alive_func, netw_members); //Most likely prone to error, terrible variable names aswell
-			pthread_create(&manage_connections_thread, NULL, &manage_connections_func, netw_members); //Investigate how sockets works as shared variables
-			pthread_create(&manage_backup_thread, NULL, &manage_backup_func, netw_members);
-			pthread_create(&distribute_orders_thread, NULL, )
+			pthread master_threads[3];
+
+			pthread_create(&master_threads[0], NULL, &broadcast_heartbeat, &shrd_vars);
+			pthread_create(&master_threads[1], NULL, &manage_netw, &shrd_vars);			//Investigate how sockets works as shared variables
+			pthread_create(&master_threads[2], NULL, &manage_backup, &shrd_vars);
 			break;
 
 		case SLAVE:
@@ -50,25 +42,27 @@ void network_fsm(){
 	}
 }
 
-int find_master(Socket_UDP* netw_members){
+int find_master(NetwMemb netw_membs[256]){
 	msg_t msg;
-	//Set timeout
-	struct timeval timeout; // Consider making the select() setup a func or try to simplify somehow
-	timeout.tv_sec = 0;
-	timeout.tv_usec = 1000000 -1; //Not sure if this can be more than a second
-	//Set processes to watch
-	fd_set readfds;
-	FD_ZERO(&readfds);
-	FD_SET(netw_members[255].pid, &readfds);
-	//Start M_ALIVE signal search
+	SelectVar slct_var = select_setup(0, 1000000 - 1, netw_membs[BROADCAST].sock_fd);
 	clock_t start = clock();
-	while (timeout.tv_usec > 0){
-		if (select(netw_members[255].pid + 1, &readfds, NULL, NULL, &timeout)){ //Not sure about first argument
-			msg = netw_members[255].recv();
-			if (MSG_ID == M_ALIVE) return msg.sender_ip;
+	while (slct_var.timeout.tv_usec > 0){
+		if (select(netw_membs[BROADCAST].sock_fd + 1, &slct_var.readfds, NULL, NULL, &slct_var.timeout)){ //Not sure about first argument
+			msg = netw_membs[BROADCAST].recv();
+			if (MSG_ID == HEARTBEAT) return msg.sender_ip;
 		}
-		timeout.tv_usec = (clock() - start) * SEC_TO_USEC / CLOCKS_PER_SEC;
+		slct_var.timeout.tv_usec = (clock() - start) * SEC_TO_USEC / CLOCKS_PER_SEC;
 	}
 	return 0;
 }
 
+SelectVar select_setup(int sec, int usec, int fd){
+	SelectVar slct_var;
+	//Set timeout
+	slct_var.timeout.tv_sec = sec;
+	slct_var.timeout.tv_usec = usec;
+	//Place the file descriptor, fd, in a file descriptor set. 
+	FD_ZERO(&slct_var.readfds);
+	FD_SET(fd, &slct_var.readfds);
+	return slct_var;
+}

0edc6488e3ad9777c1f561fc4763e84f5b7bc92e
torsteik <tkvalvaag94@hotmail.com>
2016-04-12 20:51:35 +0200

    Update netw_master.h

--- Lifts/Code/netw_master.h
+++ Lifts/Code/netw_master.h
@@ -1,25 +1,18 @@
 #ifndef NETW_MASTER_H
 #define NETW_MASTER_H
 
-#include "Socket_UDP.h"
+#include "NetwMemb.h"
 
 #define NO_BACKUP	-1
 
-struct backup_func_arg_t{ //Change name to master_shared_variables and use this for all threads in master
-	Socket_UDP*  netw_members_arg;
-	char* master_q_arg;
+struct SlaveThreadVars{			//Find better name
+	SharedVars* shrd_vars;
+	uint8_t slave_id;
 };
 
-struct shared_variables_t{
-	Socket_UDP* netw_members;
-	int ip;
-	int* master_q
-}; // Make sure this becomes a pointer and that all threads point to the same object. OBS, this is must not happen for ip_id!!!!
-//(Probably not)Also after passing this to threads it might be unnecessary to typecast it, this could be done only when needed.
-
-void* broadcast_alive_func(void* netw_members_void);
-void* manage_connections_func(void* netw_members_void);
+void* broadcast_heartbeat(void* shrd_vars_void);
+void* manage_netw(void* shrd_vars_void);
 int determine_master(); //Bad name
-void* manage_backup_func(void* );
-int init_backup();
+void* handle_orders(void* shrd_vars_void);
+void* manage_backup(void* shrd_vars_void);
 #endif

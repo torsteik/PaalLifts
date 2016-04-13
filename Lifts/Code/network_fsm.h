06fcab9ec47114ee09b9317cce88636e4dfaaa21
torsteik <tkvalvaag94@hotmail.com>
2016-04-13 22:14:02 +0200

    Update network_fsm.h

--- Lifts/Code/network_fsm.h
+++ Lifts/Code/network_fsm.h
@@ -5,24 +5,22 @@
 #include <stdint.h>
 //Most of these defines can be placed in NetwMemb, also solving or include loop problem
 #define SEC_TO_USEC		1000000
-#define N_FLOORS		4
 
 enum State{ FIND_NETWORK, MASTER, SLAVE };
+//------------NETW FSM STATES------------
+#define FSM_FIND_NETWORK	0
+#define FSM_MASTER			1
+#define FSM_SLAVE			2
 
-struct SharedVars{
-	NetwMemb netw_membs[256];
-	int master_q[N_FLOORS * 2];
-	uint8_t* slave_id;
-}; //OBS, this is must not happen for ip_id!!!!
-
-struct SelectVar{
+struct SelectVar{ //Unnecessary
 	struct timeval timeout;
 	fd_set readfds;
 };
 
-void network_fsm();
-int find_master(NetwMemb netw_membs[256]);
-SelectVar select_setup(int sec, int usec, int fd);
+
+void* netw_fsm(void* shared_void);
+unsigned long find_master(NetwMemb netw_membs[256]);
+fd_set file_descriptor_setup(int* sock_fd_set, int set_size);
 #endif
 
 
@@ -34,4 +32,17 @@ timeout.tv_usec = 400000;
 fd_set readfds;
 FD_ZERO(&readfds);
 FD_SET((shared_vars->netw_members[shared_vars->ip]).sock_fd, &readfds);  //Macro shared vars or make a new pointer or some shit
+*/
+/*
+fd_set select_setup(int sec, int usec, int sock_fd_set[], int set_size){
+	SelectVar slct_var;
+	//Set timeout
+	slct_var.timeout.tv_sec = sec;
+	slct_var.timeout.tv_usec = usec;
+	//Place the file descriptor, fd, in a file descriptor set. 
+	FD_ZERO(&slct_var.readfds);
+	for (int i = 0; i < set_size; i++){
+		FD_SET(sock_fd_set[i], &slct_var.readfds);
+	}
+	return slct_var;
 */

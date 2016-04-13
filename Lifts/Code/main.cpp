e7e364425bd1bb2de9d35be706b5bfa5aaec3669
paalhm <paalma10sen@gmail.com>
2016-04-13 09:25:19 +0200

    Added files via upload

--- Lifts/Code/main.cpp
+++ Lifts/Code/main.cpp
@@ -4,9 +4,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#include<fstream>
+#include <string>
+#include <fstream>
+#include <iostream>
 
 using namespace std;
+#define N_FLOORS 4
 
 void elevator_io_init(){
 	if (!elev_init()){
@@ -22,13 +25,46 @@ void elevator_io_init(){
 	elev_set_floor_indicator(currentFloor - 1);
 }
 
-void recover_queue(){
-	ifstream myReadFile;
-	myReadFile.open("text.txt");
-	string
+//-----------------------------------------------------------------------------------------------------------------
+
+int char_to_int(char c){
+	return (int)c - 48;
 }
+
+void recover_internal_queue(){
+
+	ifstream recovery_file("internal_recovery.txt", std::fstream::in);
+
+	if (recovery_file){
+		for (int i = 0; i < N_FLOORS; ++i){
+			internal_queue[i] = recovery_file.get();
+		}
+	}
+	else{
+		cout << "Unable to read from internal queue's recovery file" << endl;
+	}
+	recovery_file.close();
+}
+
+void write_recovery_file(){
+
+	ofstream recovery_file("internal_recovery.txt", std::fstream::out | std::fstream::trunc);
+
+	if (!recovery_file.fail()){
+		for (int i = 0; i < N_FLOORS; ++i){
+			recovery_file << internal_queue[i];
+		}
+	}
+
+	else{
+		cout << "Unable to write to the internal queue's recovery file" << endl;
+	}
+	recovery_file.close();
+}
+
+//-----------------------------------------------------------------------------------------------------------------
 
 int init(){
 	elevator_io_init();
-	recover_queue();
+	recover_internal_queue();
 }

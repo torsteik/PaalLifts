6a35e0816defb1c7c40fe95b83dd85d88ca169e4
torsteik <tkvalvaag94@hotmail.com>
2016-04-13 22:13:12 +0200

    Update network_fsm.cpp

--- Lifts/Code/network_fsm.cpp
+++ Lifts/Code/network_fsm.cpp
@@ -7,62 +7,77 @@
 #include <time.h>
 #include <stdint.h>
 
+#include "main.h"
 #include "netw_fsm.h"
 #include "netw_master.h"
 #include "NetwMemb.h"
 
-void network_fsm(){
-	SharedVars shrd_vars;																//Should maybe be named shrd_master_vars or something
-	shrd_vars.netw_membs[BROADCAST] = NetwMemb(INADDR_ANY, id_to_ip(BROADCAST), NETW_INFO_PORT, BROADCASTER); /*Review: OBS! BROADCAST need to be full IP in host byte order and BROADCASTER must be the lamest define ever, lol*/
-	for (int i = 0; i < N_FLOORS * 2; i++)	shrd_vars.master_q[i] = 0;
+void* netw_fsm(void* shared_void){
+	SharedVars* shared = (SharedVars*)shared_void;														
+	shared->netw_membs[BROADCAST] = NetwMemb(INADDR_ANY, id_to_ip(BROADCAST), NETW_INFO_PORT, BROADCAST_ROLE); 
 
-	unsigned long master_ip;
-
-	State state = FIND_NETWORK;
+	int netw_fsm_state = FSM_FIND_NETWORK;
 	while (1){
-		switch (state){
-		case FIND_NETWORK:
-			master_ip = find_master(shrd_vars.netw_membs);
-			if (!master_ip) state = MASTER; 
-			else 			state = SLAVE;
+		switch (netw_fsm_state){
+		case FSM_FIND_NETWORK:
+			unsigned long master_ip = find_master(shared->netw_membs);
+			if (!master_ip) netw_fsm_state = MASTER; 
+			else 			netw_fsm_state = SLAVE;
 			break;
 
-		case MASTER:
-			pthread master_threads[3];
+		case FSM_MASTER:
+			pthread_t master_threads[3];
 
-			pthread_create(&master_threads[0], NULL, &broadcast_heartbeat, &shrd_vars);
-			pthread_create(&master_threads[1], NULL, &manage_netw, &shrd_vars);			//Investigate how sockets works as shared variables
-			pthread_create(&master_threads[2], NULL, &manage_backup, &shrd_vars);
+			pthread_create(&master_threads[0], NULL, &broadcast_heartbeat, shared);
+			pthread_create(&master_threads[1], NULL, &manage_netw, shared);			//Investigate how sockets works as shared variables
+			pthread_create(&master_threads[2], NULL, &manage_backup, shared);
+
+
+			//Make this a function and only do it if this elev previously was a backup
+			//-------------------------------------------------------------------------
+			sleep(2);
+			for (int button = 0; button < N_FLOORS * 2; button++){
+				if (shared->netw_master_q[button] && shared->netw_membs[shared->netw_master_q[button]].role == EMPTY_SOCK){
+					shared->local_q[button] = shared->netw_master_q[button];
+				}
+			}
+			//-------------------------------------------------------------------------
 			break;
 
-		case SLAVE:
+		case FSM_SLAVE:
 
 			break;
 		}
 	}
 }
 
-int find_master(NetwMemb netw_membs[256]){
-	msg_t msg;
-	SelectVar slct_var = select_setup(0, 1000000 - 1, netw_membs[BROADCAST].sock_fd);
+unsigned long find_master(NetwMemb netw_membs[256]){
+	struct timeval timeout;
+	timeout.tv_sec  = 0;
+	timeout.tv_usec = 1000000 - 1;
+
 	clock_t start = clock();
-	while (slct_var.timeout.tv_usec > 0){
-		if (select(netw_membs[BROADCAST].sock_fd + 1, &slct_var.readfds, NULL, NULL, &slct_var.timeout)){ //Not sure about first argument
-			msg = netw_membs[BROADCAST].recv();
-			if (MSG_ID == HEARTBEAT) return msg.sender_ip;
+	while (timeout.tv_usec > 0)	{
+		
+		fd_set read_fd_set = file_descriptor_setup(&netw_membs[BROADCAST].sock_fd, 1);
+		if (select(netw_membs[BROADCAST].sock_fd + 1, &read_fd_set, NULL, NULL, &timeout)){
+			
+			msg_t msg = netw_membs[BROADCAST].recv();
+			if (msg.MSG_ID == HEARTBEAT){
+				return msg.sender_ip;
+			}
 		}
-		slct_var.timeout.tv_usec = (clock() - start) * SEC_TO_USEC / CLOCKS_PER_SEC;
+		timeout.tv_usec = (clock()-start) * SEC_TO_USEC/CLOCKS_PER_SEC;
 	}
 	return 0;
 }
 
-SelectVar select_setup(int sec, int usec, int fd){
-	SelectVar slct_var;
-	//Set timeout
-	slct_var.timeout.tv_sec = sec;
-	slct_var.timeout.tv_usec = usec;
-	//Place the file descriptor, fd, in a file descriptor set. 
-	FD_ZERO(&slct_var.readfds);
-	FD_SET(fd, &slct_var.readfds);
-	return slct_var;
+// Maybe taking in a pointer and updating it is necessary
+fd_set file_descriptor_setup(int* sock_fd_set, int set_size){ 
+	fd_set read_fd_set;
+	FD_ZERO(&read_fd_set);
+	for (int i = 0; i < set_size; i++){
+		FD_SET(sock_fd_set[i], &read_fd_set);
+	}
+	return read_fd_set;
 }

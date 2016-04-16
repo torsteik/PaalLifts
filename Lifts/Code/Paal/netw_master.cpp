8ed76146ed4531511991a901763e9bf45cfefeff
torsteik <tkvalvaag94@hotmail.com>
2016-04-16 19:19:15 +0200

    Added files via upload

--- Lifts/Code/Paal/netw_master.cpp
+++ Lifts/Code/Paal/netw_master.cpp
@@ -3,40 +3,77 @@
 #include <time.h>
 #include <sys/select.h>
 #include <pthread.h>
+#include <iostream>
 
 #include "main.h"
 #include "netw_fsm.h"	//Because: 1. struct SharedVars
 #include "netw_master.h"
 #include "NetwMemb.h"
 
-void* broadcast_heartbeat(void* shared_void){
-	SharedVars* shared = (SharedVars*)shared_void;
-	char heartbeat_msg = HEARTBEAT;
-	while (1){
-		if (!shared->netw_membs[BROADCAST].send(&heartbeat_msg))
-			printf("Failed to broadcast HEARTBEAT");
-		usleep(0.4*SEC_TO_USEC);
-	}
-}
-
 void* manage_netw(void* shared_void){
 	SharedVars* shared = (SharedVars*)shared_void;
 	pthread_t order_handling_threads[255];
 
+	pthread_create(&order_handling_threads[MY_ID], NULL, &report_events_master, shared);
+
 	while (1){
-		RecvMsg recv_msg = shared->netw_membs[BROADCAST].recv(); // Do this everywhere: recv_msg
+		struct timeval timeout;
+		timeout.tv_sec = 0;
+		timeout.tv_usec = 0.4*SEC_TO_USEC;
+
+		RecvMsg recv_msg;
+		fd_set read_fd_set = file_descriptor_setup(&shared->netw_membs[BROADCAST].sock_fd, 1);
+		if (select(shared->netw_membs[BROADCAST].sock_fd + 1, &read_fd_set, NULL, NULL, &timeout) > 0){
+			recv_msg = shared->netw_membs[BROADCAST].recv(); // Do this everywhere: recv_msg
+		}
+
 		switch (recv_msg.MSG_ID){
 		case HEARTBEAT:
 			determine_master();
 			break;
 
 		case CONNECT:
-			char send_msg = ACCEPT_CON;
+			char send_msg = ACCEPT_CON; //Ack
 			shared->netw_membs[ip_to_id(recv_msg.sender_ip)] = NetwMemb(recv_msg.sender_ip, recv_msg.sender_ip, ELEV_INFO_PORT, SLAVE_ROLE);
 			shared->netw_membs[ip_to_id(recv_msg.sender_ip)].send(&send_msg);
+
 			shared->slave_id = ip_to_id(recv_msg.sender_ip); // Need to use semaphore to keep this alive
 			pthread_create(&order_handling_threads[ip_to_id(recv_msg.sender_ip)], NULL, &handle_orders, shared);
 			break;
+		}
+
+		shared->netw_membs[BROADCAST].send_heartbeat();
+	}
+}
+
+void* report_events_master(void* shared_void){
+	SharedVars* shared = (SharedVars*)shared_void;
+
+	while (1){
+		usleep(0.1*SEC_TO_USEC);
+		if (shared->orders_new[0]){
+			char send_msg[BUFF_SIZE];
+			send_msg[0] = NEW_ORDER;
+			for (int button = 0; button < N_FLOORS * 2; button++){
+				if (!shared->netw_master_q[button] && shared->orders_new[button + 1]){
+					send_msg[1] = button;
+					uint8_t cheapest_slave = cost_fun(shared, button);
+					shared->netw_membs[cheapest_slave].send(send_msg);
+					//get ack
+					shared->netw_master_q[button] = cheapest_slave;
+				}
+			}
+			memset(shared->orders_new, 0, 1 + N_FLOORS * 2);
+		}
+
+		if (shared->orders_complete){
+			for (int button = 0; button < N_FLOORS * 2; button++){
+				if (shared->netw_master_q[button] && shared->orders_complete[button + 1]){
+					shared->netw_master_q[button] = 0;
+					//ack
+				}
+			}
+			memset(shared->orders_new, 0, 1 + N_FLOORS * 2);
 		}
 	}
 }
@@ -49,16 +86,16 @@ void* handle_orders(void* shared_void){										//HELP PLZ!
 	SharedVars* shared = (SharedVars*)shared_void;							//Fokin' names!
 	int slave_id = shared->slave_id;										//... / Release semaphore here
 
-	int lives = 3;
+	int lives = 4;
 	clock_t prev_heartbeat = clock();
 	while (1){
 		struct timeval timeout;
 		timeout.tv_sec = 0;
-		timeout.tv_usec = 400000;
+		timeout.tv_usec = 0.4*SEC_TO_USEC;
 		fd_set read_fd_set = file_descriptor_setup(&shared->netw_membs[slave_id].sock_fd, 1);
 
 		RecvMsg recv_msg;
-		if (select(shared->netw_membs[slave_id].sock_fd + 1, &read_fd_set, NULL, NULL, &timeout))
+		if (select(shared->netw_membs[slave_id].sock_fd + 1, &read_fd_set, NULL, NULL, &timeout) > 0)
 			recv_msg = shared->netw_membs[slave_id].recv();
 		else
 			recv_msg.MSG_ID = NO_RESPONSE;
@@ -67,7 +104,8 @@ void* handle_orders(void* shared_void){										//HELP PLZ!
 		case HEARTBEAT:
 			shared->netw_membs[slave_id].floor = recv_msg.content[1];
 			shared->netw_membs[slave_id].dir = recv_msg.content[2];
-			lives = 3;
+			shared->netw_membs[slave_id].elev_fsm_state = recv_msg.content[3];
+			lives = 4;
 			prev_heartbeat = clock();
 			break;
 
@@ -78,9 +116,15 @@ void* handle_orders(void* shared_void){										//HELP PLZ!
 				if (!shared->netw_master_q[button] && recv_msg.content[button + 1]){
 					send_msg[1] = button;
 					uint8_t cheapest_slave = cost_fun(shared, button);
-					shared->netw_membs[cheapest_slave].send(send_msg);
-					//get ack
-					shared->netw_master_q[button] = slave_id;
+					if (cheapest_slave == MY_ID){
+						shared->local_q[button] = 1;
+						shared->netw_master_q[button] = 255;
+					}
+					else{
+						shared->netw_membs[cheapest_slave].send(send_msg);
+						//get ack
+						shared->netw_master_q[button] = cheapest_slave;
+					}
 				}
 			}
 			break;
@@ -101,11 +145,17 @@ void* handle_orders(void* shared_void){										//HELP PLZ!
 					for (int button = 0; button < N_FLOORS * 2; button++){
 						if (shared->netw_master_q[button] == slave_id){
 							//Place order in local_q / make it re-enter the network as a new order
+							shared->orders_new[button + 1] = 1;
+							shared->orders_new[0] = NEW_ORDER;
 						}
-						close(shared->netw_membs[slave_id].sock_fd);
-						shared->netw_membs[slave_id] = NetwMemb();
-						pthread_exit(NULL);										// Might need arg for return value
 					}
+					if (shared->backup == slave_id){
+						shared->backup = NO_BACKUP;
+					}
+
+					close(shared->netw_membs[slave_id].sock_fd);
+					shared->netw_membs[slave_id] = NetwMemb();
+					pthread_exit(NULL);										// Might need arg for return value
 				}
 			}
 			break;
@@ -118,17 +168,16 @@ void* manage_backup(void* shared_void){
 	if (!shared->recovered){										// Probably needed more places
 		shared->backup = NO_BACKUP;
 	}
-	char send_msg[BUFF_SIZE];
-	for (int button = 1; button < N_FLOORS * 2 + 1; button++){
-		send_msg[button + 1] = 0;
-	}
+	char send_msg[BUFF_SIZE] = {};
 	bool update = 0;
 
 	while (1){																			//Consider using a sleep here so it doesnt use more processing power than necessary
+		usleep(0.1*SEC_TO_USEC);
 		if (shared->backup == NO_BACKUP){
-			send_msg[0] = BECOME_BACKUP;
-			for (int memb_id = 0; memb_id < 255; memb_id++){
+			send_msg[0] = TOGGLE_BACKUP;
+			for (int memb_id = 1; memb_id < 255; memb_id++){
 				if (shared->netw_membs[memb_id].role == SLAVE_ROLE){
+					send_msg[1] = memb_id;
 					shared->netw_membs[memb_id].send(send_msg);					// Need ack here and abort if not received
 					shared->backup = memb_id;
 					shared->netw_membs[memb_id].role = BACKUP_ROLE;
@@ -161,66 +210,50 @@ int cost_fun(SharedVars* shared, uint8_t new_order){
 	netw_membs[i] = [ETG,DIR]
 	*/
 	// DEFINING THINGS THAT MAKES SENSE FOR A UTILITY FUNCTION
-	int order_floor, order_dir;
+	int order_floor;
+	int order_dir;
 
-
-	//currently => up order
 	if (new_order >= N_FLOORS){
 		order_floor = new_order - N_FLOORS;
-		order_dir = 0;
+		order_dir = 1;
 	}
 	else{
 		order_floor = new_order;
-		order_dir = 1;
+		order_dir = 0;
 	}
-	int elev_floor, elev_dir;
-	int member_utilities[256];
-	for (int i = 1; i < 255; ++i){
+	int elev_floor;
+	int elev_dir;
+	int desired_dir;
+	
+	int best_index;
+	int	best_cost = INT_MAX;
+	int cost = 0;
 
-		elev_dir = (shared->netw_membs[i].dir);
-		elev_floor = shared->netw_membs[i].floor;
+	for (int i = 0; i < 255; ++i){
 
-		/*
-		if(shared->netw_memb[i].role && shared->netw_memb[i].elev_fsm_state != EMERGENCY && shared->netw_memb[i].elev_fsm_state != INIT){
-			if()
-			cost = 2*shared->netw_memb[i].elev_fsm_state*N_FLOORS;
-			cost += abs(order_floor - elev_floor);
+		cost = 0;
+
+		if (shared->netw_memb[i].role){
+			if (shared->netw_memb[i].elev_fsm_state != INIT){
+
+				elev_dir = shared->netw_membs[i].dir;
+				elev_floor = shared->netw_membs[i].floor;
+				desired_dir = (order_floor < elev_floor);
+
+				cost = (0.5*shared->netw_memb[i].elev_fsm_state + 1)*N_FLOORS;
+				cost += abs(order_floor - elev_floor);
+				if ((shared->netw_memb[i].elev_fsm_state) == MOVE && (abs(order_floor - elev_floor) == 0)){
+					cost += N_FLOOR;
+				}
+				if ((elev_dir + desired_dir + order_dir == 0) || (elev_dir + desired_dir + order_dir == 3)){
+					cost -= N_FLOORS;
+				}
 			}
 		}
-		*/
-
-
-		unsigned int cost = 1000;
-
-		
-
-		
-		else if (desired_direction < 0 && elev_dir > 0){
-			cost = N_FLOORS - elev_floor + N_FLOORS - order_floor;
-		}
-		else if (desired_direction > 0 && elev_dir >= 0){
-			cost = desired_direction;
-		}
-		else if (desired_direction > 0 && elev_dir < 0){
-			cost = elev_floor + order_floor;
-		}
-		if (!shared->netw_membs[i].dir){
-			cost--;
-		}
-		if (shared->netw_membs[i].state == EMERGENCY){
-			cost = 1000;
-		}
-		member_utilities[i] = cost;
-
-	}
-	int min_utility = 2 * N_FLOORS;
-	uint8_t min_index = 0;
-	for (int i = 1; i < 255; ++i){
-		if (member_utilities[i] < min_utility){
-			min_utility = member_utilities[i];
-			min_index = i;
+		if (cost < best_cost){
+			best_index = i;
 		}
 	}
-
-	return min_index;
+	return best_index;
 }
+

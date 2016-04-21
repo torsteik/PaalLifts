c97e62cd00d7a64a615027c87f30256ad745ba5e
torsteik <tkvalvaag94@hotmail.com>
2016-04-21 02:10:22 +0200

    Update NetwMemb.cpp

--- Lifts/Code/NetwMemb.cpp
+++ Lifts/Code/NetwMemb.cpp
@@ -27,7 +27,7 @@ NetwMemb::NetwMemb(unsigned long bind_ip, unsigned long send_ip, int port, int i
 	this->bind_addr.sin_port = htons(port);
 	printf("Our newest member: %lu\nNow known as: %i\n", 0xFFFFFFFF & bind_ip, ip_to_id(bind_ip));
 	if(broad){
-		this->bind_addr.sin_addr.s_addr = 0xFFFFFFFF & ntohl(bind_ip);
+		this->bind_addr.sin_addr.s_addr = 0xFFFFFFFF & htonl(bind_ip);
 	}
 	this->sock_setup(broad);
 
@@ -43,7 +43,6 @@ void NetwMemb::sock_setup(int broad){
 	this->sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
 	int option_value = 1;
 	if(broad){
-		printf("hei\n");
 		if (setsockopt(this->sock_fd, SOL_SOCKET, SO_BROADCAST, (void*)&option_value, sizeof(option_value)) < 0)
 			perror("setsockopt(SO_BROADCAST) failed.\n");
 	}
@@ -63,15 +62,16 @@ struct RecvMsg NetwMemb::recv(){
 		perror("Error receiving message.\n");
 		msg.MSG_ID = ERROR;
 	}
-	printf("Receiving nudes: %i\n", msg.MSG_ID);
-	printf("IP raw: %u\n", sender_addr.sin_addr.s_addr);
+	printf("Nudes: %i  from _ %i\n", msg.MSG_ID, ip_to_id(msg.sender_ip));
 	msg.sender_ip = 0xFFFFFFFF & ntohl(sender_addr.sin_addr.s_addr);
 	return msg;
 }
 
 int NetwMemb::send(char msg_content[BUFF_SIZE]){
-	printf("Sending: %i\n", msg_content[0]);
-	if (sendto(this->sock_fd, msg_content, BUFF_SIZE, 0, (struct sockaddr*) &(this->send_addr), sizeof(this->send_addr)) < 0){
+	//printf("Sending msg with ID: %i and first data: %i\n", msg_content[0], msg_content[1]);
+
+	socklen_t send_addr_size = sizeof(this->send_addr);
+	if (sendto(this->sock_fd, msg_content, BUFF_SIZE, 0, (struct sockaddr*) &(this->send_addr), send_addr_size) < 0){
 		perror("Error sending message.\n");
 		return -1;
 	}
@@ -84,13 +84,17 @@ int NetwMemb::send_and_get_ack(char msg_content[BUFF_SIZE]){
 		this->send(msg_content);
 		struct timeval timeout;
 		timeout.tv_sec  = 0;
-		timeout.tv_usec = 0.2*SEC_TO_USEC; // Might be too short
+		timeout.tv_usec = 0.4*SEC_TO_USEC; // Might be too short
 
 		fd_set read_fd_set;
 		file_descriptor_setup(&(this->sock_fd), 1, &read_fd_set);
 		if (select(this->sock_fd + 1, &read_fd_set, NULL, NULL, &timeout)){
+			if(this->netw_role == BROADCAST_ROLE){
+				struct RecvMsg echo_msg = this->recv();
+			}
 			struct RecvMsg recv_msg = this->recv();
 			if (recv_msg.MSG_ID == ACKNOWLEDGE){
+				printf("Got acknowlegment.\n");
 				return 1;
 			}
 		}
@@ -105,10 +109,10 @@ void NetwMemb::send_heartbeat(char netw_master_q[N_FLOORS * 2], struct timeval t
 	prev_heartbeat += (0.2*SEC_TO_USEC - timeout_leftovers.tv_usec);
 	
 	if (prev_heartbeat > 0.5*SEC_TO_USEC){
-		char heartbeat[BUFF_SIZE];
+		char heartbeat[BUFF_SIZE] = {};
 		heartbeat[0] = HEARTBEAT;
 		
-		if (this->netw_role == SLAVE_ROLE){
+		if (this->netw_role == MASTER_ROLE){
 			heartbeat[1] = this->floor;
 			heartbeat[2] = this->dir;
 			heartbeat[3] = this->elev_fsm_state;
